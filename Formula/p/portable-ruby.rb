35ab1d2743bb9bac4df65ebeb7bf0b85ddd391fe
Michael Cho <michael@michaelcho.dev>
2026-07-12 11:42:56 -0400

    portable-ruby: test

--- Formula/p/portable-ruby.rb
+++ Formula/p/portable-ruby.rb
@@ -6,7 +6,7 @@ class PortableRuby < PortableFormula
   url "https://cache.ruby-lang.org/pub/ruby/4.0/ruby-4.0.5.tar.gz"
   sha256 "7d6149079a63f8ae1d326c9fa65c6019ba2dc3155eae7b39159817911c88958e"
   license "Ruby"
-  revision 1
+  revision 2
 
   # This regex restricts matching to versions other than X.Y.0.
   livecheck do
